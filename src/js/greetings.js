fc5c4c46fb973cda7bf23aefaf7ba804ad7f060b
Peca Lee <studiopeca.lee@gmail.com>
2020-09-06 16:01:31 +0900

    updateGreeting

--- src/js/greetings.js
+++ src/js/greetings.js
@@ -1,67 +1,67 @@
 const nameDiv = document.querySelector(".name"),
-    nameForm = nameDiv.querySelector(".nameForm"),
-    nameInput = nameForm.querySelector("input"),
-    greetings = nameDiv.querySelector("h3");
+  nameForm = nameDiv.querySelector(".nameForm"),
+  nameInput = nameForm.querySelector("input"),
+  greetings = nameDiv.querySelector("h3");
 
 const USER_NAME = "userName",
-    SHOW_ELEMENT = "showing";
+  SHOW_ELEMENT = "showing";
 
 function saveUser(value) {
-    localStorage.setItem(USER_NAME, value);
+  localStorage.setItem(USER_NAME, value);
 }
 
 function getTime() {
-    const date = new Date();
-    const hours = date.getHours();
-    return hours;
+  const date = new Date();
+  const hours = date.getHours();
+  return hours;
 }
 
 function greetingComents() {
-    const hours = getTime();
-    let greetingComents;
-    if (4 < hours && hours < 12) {
-        greetingComents = "Good morning!";
-    } else if (12 <= hours && hours < 17) {
-        greetingComents = "Good afternoon!";
-    } else if (17 <= hours && hours < 21) {
-        greetingComents = "Good evening!";
-    } else {
-        greetingComents = "Good night!";
-    }
-    return greetingComents;
+  const hours = getTime();
+  let greetingComents;
+  if (4 < hours && hours < 12) {
+    greetingComents = "Good morning!";
+  } else if (12 <= hours && hours < 17) {
+    greetingComents = "Good afternoon!";
+  } else if (17 <= hours && hours < 21) {
+    greetingComents = "Good evening!";
+  } else {
+    greetingComents = "Good night!";
+  }
+  return greetingComents;
 }
 
 function paintUser() {
-    nameForm.classList.remove(SHOW_ELEMENT);
-    greetings.classList.add(SHOW_ELEMENT);
-    const userName = localStorage.getItem(USER_NAME),
-        greetingComent = greetingComents();
-    greetings.innerText = `${greetingComent} ${userName}`;
+  nameForm.classList.remove(SHOW_ELEMENT);
+  greetings.classList.add(SHOW_ELEMENT);
+  const userName = localStorage.getItem(USER_NAME),
+    greetingComent = greetingComents();
+  greetings.innerText = `${greetingComent} ${userName}`;
 }
 
 function handleSubmit(event) {
-    event.preventDefault();
-    const userName = nameInput.value;
-    saveUser(userName);
-    paintUser();
+  event.preventDefault();
+  const userName = nameInput.value;
+  saveUser(userName);
+  paintUser();
 }
 
 function askUserName() {
-    nameForm.classList.add(SHOW_ELEMENT);
-    nameForm.addEventListener("submit", handleSubmit);
+  nameForm.classList.add(SHOW_ELEMENT);
+  nameForm.addEventListener("submit", handleSubmit);
 }
 
 function loadName() {
-    const userName = localStorage.getItem(USER_NAME);
-    if (userName === null) {
-        askUserName();
-    } else {
-        paintUser();
-    }
+  const userName = localStorage.getItem(USER_NAME);
+  if (userName === null) {
+    askUserName();
+  } else {
+    paintUser();
+  }
 }
 
 function init() {
-    loadName();
+  loadName();
 }
 
 init();
